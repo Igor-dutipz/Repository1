e32948150fa429e8e75d0028ad09bfc33a35c882
Igor <dutipz@ukr.net>
2019-08-21 17:00:08 +0300

    Done

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
 
             Console.ReadKey();
         }
